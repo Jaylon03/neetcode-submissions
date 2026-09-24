1c86c2497206a76174735da71cf4156745382dfb
Jaylon Nelson <jaylonnell03@gmail.com>
2026-09-23 23:49:26 -0500

    Add: anagram-groups - submission-25

---
+++ Data Structures & Algorithms/anagram-groups/submission-25.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> contains = new HashMap<>();
+        
+        for(String str : strs){
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            if(!contains.containsKey(key)){
+                contains.put(key, new ArrayList<>());
+            }
+
+            contains.get(key).add(str);
+        }
+
+        return new ArrayList<>(contains.values());
+        
+    }
+}
+
